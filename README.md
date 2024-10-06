ad030822056a696dfdf46bc409bbb05be024f00b
Leo-alves2 <leo.alves.b.vieira@gmail.com>
2024-10-06 10:42:14 -0300

    Atualização

---
+++ README.md
@@ -0,0 +1,8 @@
+# Explicação Geral
+
+Este código cria uma aplicação web interativa utilizando o **Streamlit** para visualização de dados de preços de combustíveis no Brasil de 2013 a 2024. A aplicação permite ao usuário:
+- Selecionar o tipo de combustível e o estado de interesse.
+- Visualizar um gráfico interativo da evolução dos preços ao longo do tempo.
+- O gráfico é gerado usando a biblioteca **Altair** para visualização de dados.
+
+Além disso, há uma barra lateral para seleção de filtros e a exibição de um logotipo da empresa usando a biblioteca **Pillow** para manipulação de imagens.
--- main.py
+++ main.py
@@ -1,64 +1,86 @@
-import streamlit as st
-import pandas as pd
-import altair as alt
-from PIL import Image
+# IMPORTAR BIBLIOTECAS
+import streamlit as st  # Para criar a interface interativa
+import pandas as pd     # Para manipulação de dados
+import altair as alt    # Para criar gráficos interativos
+from PIL import Image   # Para manipular e exibir imagens
 
+# CONFIGURAÇÃO DA PÁGINA
+# Define o layout da página como 'wide' (amplo) para otimizar o uso do espaço na tela
 st.set_page_config(layout='wide')
 
+# FUNÇÃO PARA GERAR O DATAFRAME
+# A função 'gerar_df' carrega os dados de um arquivo Excel e retorna um dataframe pandas
+# O decorador `@st.cache_data` garante que a função seja executada apenas uma vez e os dados sejam armazenados em cache para melhorar o desempenho
 @st.cache_data
 def gerar_df():
     df = pd.read_excel(
-        io = "database.xlsx",
-        engine = "openpyxl",
-        sheet_name= "Pasta1",
-        usecols= "A:Q",
-        nrows= 22657
+        io="database.xlsx",       # Caminho para o arquivo Excel
+        engine="openpyxl",        # Motor necessário para ler o arquivo Excel
+        sheet_name="Pasta1",      # Nome da aba que contém os dados
+        usecols="A:Q",            # Colunas a serem carregadas
+        nrows=22657               # Número de linhas a serem carregadas
     )
     return df
 
+# CARREGANDO OS DADOS
 df = gerar_df()
 
+# FILTRANDO COLUNAS ÚTEIS
+# Mantém apenas as colunas essenciais para a análise
 colunasUteis = ['MÊS', 'PRODUTO', 'REGIÃO', 'ESTADO', 'PREÇO MÉDIO REVENDA']
-
 df = df[colunasUteis]
 
+# CONFIGURAR A BARRA LATERAL (SIDEBAR)
 with st.sidebar:
-    st.subheader('PL Data Analysis')
-    logo = Image.open('LogoPL.png')
-    st.image(logo, use_column_width=True)
-    st.subheader('Seleção de Filtros')
+    st.subheader('PL Data Analysis')  # Título da barra lateral
+    # Exibe o logo da empresa
+    logo = Image.open('LogoPL.png')  # Abre a imagem do logo
+    st.image(logo, use_column_width=True)  # Exibe a imagem com o ajuste automático de largura
+    
+    st.subheader('Seleção de Filtros')  # Seção para seleção de filtros
+    # Filtro de seleção do tipo de combustível
     fProduto = st.selectbox(
-        'Selecione o combustível',
-        options= df['PRODUTO'].unique()
+        'Selecione o combustível',  # Rótulo do selectbox
+        options=df['PRODUTO'].unique()  # Opções são únicas da coluna 'PRODUTO'
     )
-
+    
+    # Filtro de seleção do estado
     fEstado = st.selectbox(
-        'Selecione o Estado',
-        options= df['ESTADO'].unique()
+        'Selecione o Estado',  # Rótulo do selectbox
+        options=df['ESTADO'].unique()  # Opções são únicas da coluna 'ESTADO'
     )
-
+    
+    # Filtra os dados de acordo com o combustível e o estado selecionados pelo usuário
     dadosUsuario = df.loc[(
-        df['PRODUTO'] == fProduto) &
+        df['PRODUTO'] == fProduto) & 
         (df['ESTADO'] == fEstado)
     ]
 
+# FORMATAÇÃO DA DATA
+# Converte a coluna 'MÊS' para o formato ano/mês e altera a formatação do mês
 updateDatas = dadosUsuario['MÊS'].dt.strftime('%Y/%b')
-
 dadosUsuario['MÊS'] = updateDatas[0:]
 
-st.header('Preços dos Combustíveis no Brasil: 2013 à 2024')
+# EXIBIÇÃO DOS DADOS NA PÁGINA PRINCIPAL
+st.header('Preços dos Combustíveis no Brasil: 2013 à 2024')  # Cabeçalho principal da página
+# Exibe o combustível selecionado pelo usuário
 st.markdown('**Combustível selecionado :**  ' + fProduto)
+# Exibe o estado selecionado pelo usuário
 st.markdown('**Estado selecionado :**  ' + fEstado)
 
+# CRIAÇÃO DO GRÁFICO INTERATIVO
+# O gráfico de linhas mostra a evolução do preço médio de revenda por mês para o combustível e estado selecionados
 grafCombEstado = alt.Chart(dadosUsuario).mark_line(
-    point= alt.OverlayMarkDef(color='red', size=20)
+    # Adiciona pontos vermelhos nos dados do gráfico
+    point=alt.OverlayMarkDef(color='red', size=20)
 ).encode(
-    x = 'MÊS:T',
-    y = 'PREÇO MÉDIO REVENDA',
-    strokeWidth = alt.value(3)
+    x='MÊS:T',  # Eixo X representa o mês
+    y='PREÇO MÉDIO REVENDA',  # Eixo Y representa o preço médio de revenda
+    strokeWidth=alt.value(3)  # Define a largura da linha do gráfico
 ).properties(
-    height = 700,
-    width = 1000
+    height=700,  # Altura do gráfico
+    width=1000   # Largura do gráfico
 )
 
-st.altair_chart(grafCombEstado)
+# EXIBIÇÃO DO GRÁFICO NA PÁGINA
+st.altair_chart(grafCombEstado)  # Exibe o gráfico Altair na página
